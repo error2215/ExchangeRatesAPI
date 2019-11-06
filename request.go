2601ea1e2188425b26e0518b4c99a2ed7a241d96
misha <slyvkamm@gmail.com>
2019-11-06 19:10:21 +0200

    debug mode

--- request.go
+++ request.go
@@ -1,11 +1,15 @@
 package ExchangeRatesAPI
 
 import (
+	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 )
 
-func (a *ExchangeRatesAPI) Get() (string, error) {
+func (a *ExchangeRatesAPI) Get(debug bool) (string, error) {
+	if debug {
+		logrus.Info("Query:" + a.buildQuery())
+	}
 	resp, err := http.Get(a.apiURL + a.buildQuery())
 	if err != nil {
 		return "", err
